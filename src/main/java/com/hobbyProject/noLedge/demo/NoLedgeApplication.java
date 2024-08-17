5878943c32e7ffb71e8fdb1fa8bb15457c9a1dfc
nikwoz <niklaswozlop@gmail.com>
2024-08-17 11:29:53 +0200

    Strarting spring boot app

---
+++ src/main/java/com/hobbyProject/noLedge/demo/NoLedgeApplication.java
@@ -0,0 +1,13 @@
+package com.hobbyProject.noLedge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoLedgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoLedgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobbyProject/noLedge/demo/NoLedgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyProject.noLedge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoLedgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
